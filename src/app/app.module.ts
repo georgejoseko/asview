71babb755aa8bf468d17b0734d7b2517382a082c
faridullah1 <faridullah996@gmail.com>
2023-10-08 21:27:23 +0500

    component addition to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FileSaverModule } from 'ngx-filesaver';
 
-import {MatSelectModule} from '@angular/material/select';
-import {MatToolbarModule} from '@angular/material/toolbar'; 
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material/form-field'; 
-import {MatCardModule} from '@angular/material/card';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatInputModule} from '@angular/material/input'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,8 +23,15 @@ import { AddsystemComponent } from './addsystem/addsystem.component';
 import { AddswComponent } from './addsw/addsw.component';
 import { AddsyststComponent } from './addsystst/addsystst.component';
 import { AddswtstComponent } from './addswtst/addswtst.component';
-import { ConfirmationComponent } from './confirm-dialog/confirm-dialog.component';
-
+import { AnalysisComponent } from './analysis/analysis.component';
+import { AnalysisFormComponent } from './analysis/form/analysis-form.component';
+import { InfoComponent } from './info-dialog/info-dialog.component';
+import { ReportComponent } from './report/report.component';
+import { ArchitectureComponent } from './architecture/architecture.component';
+import { ArchitectureFormComponent } from './architecture/form/architecture-form.component';
+import { HierarchyComponent } from './architecture/hierarchy/hierarchy.component';
+import { HierarchyModule } from './architecture/hierarchy/hierarchy.module';
+import { ConfirmationModule } from './confirm-dialog/confirm-dialog.module';
 
 @NgModule({
   declarations: [
@@ -34,7 +42,13 @@ import { ConfirmationComponent } from './confirm-dialog/confirm-dialog.component
     AddswComponent,
     AddsyststComponent,
     AddswtstComponent,
-    ConfirmationComponent
+    AnalysisComponent,
+    AnalysisFormComponent,
+    ReportComponent,
+    ArchitectureComponent,
+    ArchitectureFormComponent,
+    HierarchyComponent,
+    InfoComponent,
   ],
   imports: [
     BrowserModule,
@@ -50,9 +64,12 @@ import { ConfirmationComponent } from './confirm-dialog/confirm-dialog.component
     MatExpansionModule,
     ReactiveFormsModule,
     FormsModule,
-    FileSaverModule
+    FileSaverModule,
+    MatTableModule,
+    MatTabsModule,
+    HierarchyModule,
+    ConfirmationModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
